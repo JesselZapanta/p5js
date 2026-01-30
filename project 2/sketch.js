217d774a8d1f92e549457593f1694cd519f89042
JesselZapanta <jesselzapanta9@gmail.com>
2026-01-30 14:33:50 +0800

    edit params

--- project 2/sketch.js
+++ project 2/sketch.js
@@ -11,14 +11,19 @@ function draw() {
   // put drawing code here
   // console.log('Hello from draw');
   stroke('blue');
+  strokeWeight(5);
   // line(x1, y1, x2, y2);
   line(50, 50, 350, 350);
   line(350, 50, 50, 350);
 
   stroke('red');
-  strokeWeight(5);
   line(50, 50, 350, 50);
   line(350, 50, 350, 350);
   line(50, 350, 350, 350);
   line(50, 350, 50, 50);
+
+  // point(x, y);
+  strokeWeight(20);
+  stroke(0);
+  point(50,50)
 }
