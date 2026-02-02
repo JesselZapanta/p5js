54320187b41ef8451bf6118f213bd3bcd79494bf
JesselZapanta <jesselzapanta9@gmail.com>
2026-02-02 14:28:21 +0800

    circle

--- project 2/sketch.js
+++ project 2/sketch.js
@@ -1,29 +1,57 @@
-function setup() {
- // put setup code here
-  // createCanvas(h, w);
-  createCanvas(400, 400);
-  // background('white');//string 
-  background(255);//grayscale 
-  // background(255, 255, 255);//rgb 
-}
-// ---------------------------
-function draw() {
-  // put drawing code here
-  // console.log('Hello from draw');
-  stroke('blue');
-  strokeWeight(5);
-  // line(x1, y1, x2, y2);
-  line(50, 50, 350, 350);
-  line(350, 50, 50, 350);
+  function setup() {
+  // put setup code here
+    // createCanvas(h, w);
+    createCanvas(400, 400);
+    // background('white');//string 
+    background(255);//grayscale 
+    // background(255, 255, 255);//rgb 
+  }
+  // ---------------------------
+  function draw() {
+    // put drawing code here
+    // console.log('Hello from draw');
+    // stroke('blue');
+    // strokeWeight(5);
+    // // line(x1, y1, x2, y2);
+    // line(50, 50, 350, 350);
+    // line(350, 50, 50, 350);
+    // line(200, 50, 200, 350);
+    // line(50, 200, 350, 200);
 
-  stroke('red');
-  line(50, 50, 350, 50);
-  line(350, 50, 350, 350);
-  line(50, 350, 350, 350);
-  line(50, 350, 50, 50);
+    // stroke('red');
+    // line(50, 50, 350, 50);
+    // line(350, 50, 350, 350);
+    // line(50, 350, 350, 350);
+    // line(50, 350, 50, 50);
 
-  // point(x, y);
-  strokeWeight(20);
-  stroke(0);
-  point(50,50)
-}
+
+    // point(x, y);
+    // strokeWeight(20);
+    // stroke(0);
+    // point(50,50)
+    // point(350,50)
+    // point(350,350)
+    // point(50,350)
+    // point(200,200)
+    // point(200,50)
+    // point(350,200)
+    // point(200,350) 
+    // point(50,200)
+    
+    // fill('red');
+    // stroke('blue');
+    // strokeWeight(4);
+    // // noStroke();
+    // circle(200, 200, 50);
+
+    fill('red');
+    circle(75, 100, 100);
+    fill('blue');
+    circle(200, 100, 100);
+    fill('orange');
+    circle(325, 100, 100);
+    fill('brown');
+    circle(75, 225, 100);
+    circle(200, 225, 100);
+    circle(325, 225, 100);
+  }
